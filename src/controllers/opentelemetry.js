799a45e6c3053add0e9d295f95e8c4b365dd8126
tokebe <43009413+tokebe@users.noreply.github.com>
2023-12-14 15:46:34 -0500

    chore: format, debug statements, rm metrics

--- src/controllers/opentelemetry.js
+++ src/controllers/opentelemetry.js
@@ -1,34 +1,22 @@
-/*instrumentation.js*/
-const opentelemetry = require('@opentelemetry/sdk-node');
-const {
-  getNodeAutoInstrumentations,
-} = require('@opentelemetry/auto-instrumentations-node');
-const {
-  OTLPTraceExporter,
-} = require('@opentelemetry/exporter-trace-otlp-proto');
-const {
-  OTLPMetricExporter,
-} = require('@opentelemetry/exporter-metrics-otlp-proto');
-const { PeriodicExportingMetricReader, ConsoleMetricExporter } = require('@opentelemetry/sdk-metrics');
-const { Resource } = require('@opentelemetry/resources')
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
+const opentelemetry = require("@opentelemetry/sdk-node");
+const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
+const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-proto");
+const { OTLPMetricExporter } = require("@opentelemetry/exporter-metrics-otlp-proto");
+const { PeriodicExportingMetricReader, ConsoleMetricExporter } = require("@opentelemetry/sdk-metrics");
+const { Resource } = require("@opentelemetry/resources");
+const { ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-node");
+const Debug = require("debug");
+const debug = Debug("bte:biothings-explorer:otel-init");
 
+debug("Initializing Opentelemetry instrumentation...");
 const sdk = new opentelemetry.NodeSDK({
   traceExporter: new OTLPTraceExporter({
-    url: process.env.JAEGER_URL ?? 'http://localhost:4318/v1/traces',
-    // optional - collection of custom headers to be sent with each request, empty by default
-    headers: {},
-  }),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new OTLPMetricExporter({
-      url: process.env.JAEGER_URL ?? 'http://localhost:4318/v1/traces',
-      headers: {}, // an optional object containing custom headers to be sent with each request
-      concurrencyLimit: 1, // an optional limit on pending requests
-    }),
+    url: process.env.JAEGER_URL ?? "http://localhost:4318/v1/traces",
   }),
   instrumentations: [getNodeAutoInstrumentations()],
   resource: new Resource({
-    "service.name": "biothings-explorer"
-  })
+    "service.name": "biothings-explorer",
+  }),
 });
 sdk.start();
+debug("Opentelemetry instrumentation initialized.");
