f0873e9bcda8e82414b0b1509b3ee989f0b926c3
tokebe <43009413+tokebe@users.noreply.github.com>
2024-01-18 12:05:17 -0500

    mild changes

--- src/controllers/opentelemetry.js
+++ src/controllers/opentelemetry.js
@@ -1,20 +1,20 @@
 const opentelemetry = require("@opentelemetry/sdk-node");
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
 const { Resource } = require("@opentelemetry/resources");
-const { isMainThread } = require('worker_threads');
+const { isMainThread } = require("worker_threads");
 const Debug = require("debug");
 const debug = Debug("bte:biothings-explorer:otel-init");
-const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
+const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-proto");
 
 debug("Initializing Opentelemetry instrumentation...");
 const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new JaegerExporter({
+  traceExporter: new OTLPTraceExporter({
     host: process.env.JAEGER_HOST ?? "jaeger-otel-agent.sri",
     port: parseInt(process.env.JAEGER_PORT ?? "6832"),
   }),
-  instrumentations: isMainThread ? [getNodeAutoInstrumentations()] : [],
+  instrumentations: [getNodeAutoInstrumentations()],
   resource: new Resource({
-    "service.name": isMainThread ? "biothings-explorer" : "biothings-explorer-thread",
+    "service.name": "biothings-explorer",
   }),
 });
 sdk.start();
