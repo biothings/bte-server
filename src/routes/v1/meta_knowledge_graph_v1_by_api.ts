6518716b49ecf144c80bbe0f1cc7caa2e7ceac49
NeuralFlux <40491005+NeuralFlux@users.noreply.github.com>
2024-10-31 13:19:23 -0400

    fix: update team and api metakg endpoints to use globally loaded graph in async fashion

--- src/routes/v1/meta_knowledge_graph_v1_by_api.ts
+++ src/routes/v1/meta_knowledge_graph_v1_by_api.ts
@@ -1,22 +1,41 @@
 import handler from "../../controllers/meta_knowledge_graph";
 import * as utils from "../../utils/common";
+import path from "path";
+import { TaskInfo } from "@biothings-explorer/types";
+import { runTask, taskResponse, taskError } from "../../controllers/threading/threadHandler";
 import { Express, NextFunction, Request, Response, RequestHandler } from "express";
+
+import MetaKnowledgeGraph from "@biothings-explorer/smartapi-kg";
 
 class MetaKGByAPI {
   setRoutes(app: Express) {
     app
-      .route("/v1/smartapi/:smartapiID/meta_knowledge_graph")
+      .route("/v1/smartapi/:smartAPIID/meta_knowledge_graph")
       .get((async (req: Request, res: Response, next: NextFunction) => {
         try {
-          const metaKGHandler = new handler(req.params.smartapiID);
-          const kg = await metaKGHandler.getKG();
+          const response = await runTask(req, res, path.parse(__filename).name);
           res.setHeader("Content-Type", "application/json");
-          res.end(JSON.stringify(kg));
+          res.end(JSON.stringify(response));
         } catch (error) {
           next(error);
         }
       })as RequestHandler)
       .all(utils.methodNotAllowed);
+  }
+
+  async task(taskInfo: TaskInfo) {
+    try {
+      const metaKGHandler = new handler(taskInfo.data.smartAPIID, undefined);
+      let metakg = undefined;
+      // initialize MetaKG only if ops are provided because handler logic is built upon that
+      if (taskInfo.data.options.metakg_ops !== undefined)
+        metakg = new MetaKnowledgeGraph(undefined, undefined, taskInfo.data.options.metakg_ops);
+      const kg = await metaKGHandler.getKG(metakg);
+      // response.logs = utils.filterForLogLevel(response.logs, options.logLevel);
+      return taskResponse(kg);
+    } catch (error) {
+      taskError(error as Error);
+    }
   }
 }
 
